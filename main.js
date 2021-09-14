f34d3b0080178d0db6f40b30e20e236812559f52
Raul Almeida <77411564+devraulalmeida7@users.noreply.github.com>
2021-09-14 20:55:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var nome = "Raul";
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 9
+var notaDoTerceiroBimestre = 10
+var notaDoQuartoBimestre = 9.5
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) /4
+
+var notaFixada = notaFinal.toFixed(1)
+console.log(notaFinal);
+console.log(notaFixada)
+
+// https://codepen.io/luzhnikidev12/pen/OJgjWXz?editors=0112
